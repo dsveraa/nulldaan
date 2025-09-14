98b447b9eedf552ddd4aac384c321f6b478f0320
dsveraa <dsvera.a@gmail.com>
2025-09-14 19:26:07 -0300

    feat: delete account

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,44 +1,50 @@
 from flask import request, jsonify, Blueprint
-
-from app.services.auth_services import Logout, UserAuth, UserRegistration
+from app.services.auth_services import Logout, AccountManagement, SignUp
 
 
 auth_bp = Blueprint('auth', __name__)
 
 
-@auth_bp.route("/login", methods=["POST"])
+@auth_bp.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
     email = data.get('email')
     password = data.get('password')
     
-    user_auth = UserAuth(email, password)
-    authorized = user_auth.auth()
-    username = user_auth.get_username()
+    account = AccountManagement(email, password)
+    authorized = account.login()
+    username = account.get_username()
 
     if authorized:
-        return jsonify({"status": "ok", "username": username }), 200
+        return jsonify({'status': 'ok', 'username': username}), 200
     else:
-        return jsonify ({"status": "error"}), 401
+        return jsonify ({'status': 'error'}), 401
 
 
-@auth_bp.route("/logout")
+@auth_bp.route('/logout')
 def logout():
     logout_query = Logout()
-    return jsonify({ "response": logout_query })
+    return jsonify({'response': logout_query}), 200
 
 
-@auth_bp.route("/signup", methods=["POST"])
+@auth_bp.route('/signup', methods=['POST'])
 def signup():
     data = request.get_json()
     name = data.get('name')
     email = data.get('email')
     password = data.get('password')
     
-    registration = UserRegistration(name, email, password)
-    result = registration.register()
+    signup = SignUp(name, email, password)
+    result = signup.signup()
 
     if result['status'] == 'error':
         return jsonify(result), 400
 
-    return jsonify(result)
+    return jsonify(result), 201
+
+
+@auth_bp.route('/account/<email>', methods=['DELETE'])
+def delete_account(email):
+    account = AccountManagement(email)
+    account.delete_user()    
+    return 'deleted', 204
--- app/services/auth_services.py
+++ app/services/auth_services.py
@@ -3,13 +3,13 @@ from flask import session
 from werkzeug.security import generate_password_hash, check_password_hash
 from .. import db
 
-class UserAuth:
-    def __init__(self, email, password):
+class AccountManagement:
+    def __init__(self, email, password=None):
         self.email = email
         self.password = password
         self.user = Users.query.filter_by(email=email).first()
 
-    def auth(self):
+    def login(self):
         if self.user and check_password_hash(self.user.password, self.password):
             session["user_id"] = self.user.id
             session["username"] = self.user.name
@@ -19,6 +19,12 @@ class UserAuth:
 
     def get_username(self):
         return self.user.name
+    
+    def delete_user(self):
+        username = self.get_username()
+        db.session.delete(self.user)
+        db.session.commit()
+        return {"status": "deleted", "user": username}
 
 
 class Logout:
@@ -28,13 +34,13 @@ class Logout:
         return True
 
 
-class UserRegistration:
+class SignUp:
     def __init__(self, name, email, password):
         self.name = name
         self.email = email
         self.password = password
         
-    def register(self):
+    def signup(self):
         if Users.query.filter_by(email=self.email).first():
             return {"status": "error", "message": "Email already exists."}
     
@@ -43,4 +49,4 @@ class UserRegistration:
         db.session.add(new_user)
         db.session.commit()
         return {"status": "ok"}
-    
+
