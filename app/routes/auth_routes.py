75a8e7a99f2f84baa3b467397c6b3e5cdca35e2e
dsveraa <dsvera.a@gmail.com>
2025-09-14 18:41:52 -0300

    feat: /logout and /signup routes

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify, Blueprint
 
-from app.services.auth_services import UserAuth
+from app.services.auth_services import Logout, UserAuth, UserRegistration
 
 
 auth_bp = Blueprint('auth', __name__)
@@ -14,8 +14,31 @@ def login():
     
     user_auth = UserAuth(email, password)
     authorized = user_auth.auth()
+    username = user_auth.get_username()
 
     if authorized:
-        return jsonify({"status": "ok"}), 200
+        return jsonify({"status": "ok", "username": username }), 200
     else:
         return jsonify ({"status": "error"}), 401
+
+
+@auth_bp.route("/logout")
+def logout():
+    logout_query = Logout()
+    return jsonify({ "response": logout_query })
+
+
+@auth_bp.route("/signup", methods=["POST"])
+def signup():
+    data = request.get_json()
+    name = data.get('name')
+    email = data.get('email')
+    password = data.get('password')
+    
+    registration = UserRegistration(name, email, password)
+    result = registration.register()
+
+    if result['status'] == 'error':
+        return jsonify(result), 400
+
+    return jsonify(result)
--- app/services/auth_services.py
+++ app/services/auth_services.py
@@ -1,6 +1,7 @@
 from app.models import Users
 from flask import session
 from werkzeug.security import generate_password_hash, check_password_hash
+from .. import db
 
 class UserAuth:
     def __init__(self, email, password):
@@ -8,14 +9,38 @@ class UserAuth:
         self.password = password
         self.user = Users.query.filter_by(email=email).first()
 
-    
     def auth(self):
-        # if self.user and check_password_hash(self.user.password, self.password):
-        if self.user and self.user.password == self.password:
+        if self.user and check_password_hash(self.user.password, self.password):
             session["user_id"] = self.user.id
             session["username"] = self.user.name
             session.permanent = True
-
             return True
         return False
+
+    def get_username(self):
+        return self.user.name
+
+
+class Logout:
+    def __init__(self):
+        session.pop("user_id", None)
+        session.pop("username", None)
+        return True
+
+
+class UserRegistration:
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+        
+    def register(self):
+        if Users.query.filter_by(email=self.email).first():
+            return {"status": "error", "message": "Email already exists."}
+    
+        password_hash = generate_password_hash(self.password)
+        new_user = Users(name=self.name, email=self.email, password=password_hash)
+        db.session.add(new_user)
+        db.session.commit()
+        return {"status": "ok"}
     
